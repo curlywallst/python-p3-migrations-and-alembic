dacaddae5732e673a9336458e64c169c003aa216
github username <github email address>
2022-09-14 18:10:54 -0400

    done

---
+++ P3/migrations/versions/3e75a59536e6_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3e75a59536e6
+Revises: 
+Create Date: 2022-09-14 17:27:01.864587
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3e75a59536e6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
